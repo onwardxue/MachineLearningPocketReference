db260bc496abacf40afc4febe5cbf434c9efe40d
xuebinbin <xuebinbin@xuebinbindeMacBook-Air.local>
2022-06-20 22:20:25 +0800

    Initial commit

---
+++ Content/Chart1.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+# @Time : 2022/6/20 10:05 下午
+# @Author : Bin Bin Xue
+# @File : Chart1
+# @Project : Machine Learning Pocket Reference
+
+# 1.安装库
+# 1.1 pip安装
+# 1.1.1 安装虚拟环境
+# python -m env env
+# 1.1.2 激活虚拟环境
+# mac&linux:
+# source env/bin/activate
+# windows:
+# C:> env\Scripts\activate.bat
+# 1.1.3 安装包
+# pip install ...
+# pip search ...（包名与库名不同的时候用）
+# pip free > requirement （批量导包）
+# pip install -r requirements.txt
+#
+# 1.2 conda安装
+# conda create --name env python=3.6
+# conda activate env
+# conda search ...
+# conda install ...
+# conda env export > environment.yml
+# conda create -f environment.yml
---
+++ Content/Chart2.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+# @Time : 2022/6/20 10:18 下午
+# @Author : Bin Bin Xue
+# @File : Chart2
+# @Project : Machine Learning Pocket Reference
+
